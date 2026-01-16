2665bd2442492f625c1e5fa2c4695786fcb6c1fa
reberojacques25 <reberojacques25@gmail.com>
2026-01-16 15:15:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if not GEMINI_API_KEY:
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # ----------------------------------
 # SIDEBAR
